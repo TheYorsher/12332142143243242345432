02c2b0bc997f607041f922495d2e6786ba9c071f
Pvl <stepanov2003@mail.ru>
2019-11-14 17:08:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int a = 100;
+        int b = 200;
+        int g = a + b;
+        int l = g + b;
+    }
+}
